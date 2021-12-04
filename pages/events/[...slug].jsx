ad0e27bfb706eb0eaa2a53641bf056c7513d5621
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-04 14:05:53 +0530

    Added a filter form to filter the events by year and month

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,9 +1,45 @@
+import { getFilteredEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+
 const FilteredEventsPage = () => {
-  return (
-    <div>
-      <h1>This is Slug Page</h1>
-    </div>
-  );
+  const router = useRouter();
+
+  const lst = router.query.slug;
+
+  if (!lst) {
+    return <p className="center">Loading...</p>;
+  }
+
+  let year = lst[0];
+  let month = lst[1];
+
+  const numYear = +year;
+  const numMonth = +month;
+
+  if (
+    isNaN(numMonth) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p className="center">Invalid Filter please adjust your values</p>;
+  }
+
+  const dateFilter = {
+    year: numYear,
+    month: numMonth,
+  };
+
+  const filteredEvents = getFilteredEvents(dateFilter);
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p className="center">No events found for the chosen filter.</p>;
+  }
+
+  return <EventList items={filteredEvents} />;
 };
 
 export default FilteredEventsPage;
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,13 +1,24 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
 
+  const router = useRouter();
+
+  const FindEventsHandler = (selectedYear, selectedMonth) => {
+    router.push(`/events/${selectedYear}/${selectedMonth}`);
+  };
+
   return (
-    <div>
+    <Fragment>
+      <EventsSearch onSearch={FindEventsHandler} />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 };
 
