9ae3adf25699f375de90cb0566484c19ad7a6674
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-06 09:04:53 +0530

    Adding Head content using next/head

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,18 @@
+import Head from "next/head";
+
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 
 const HomePage = (props) => {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
@@ -21,3 +30,5 @@ export async function getStaticProps(context) {
     revalidate: 1800,
   };
 }
+
+// You can add the Head tag from next.js anywhere inside the jsx code and it will add it to the top of the page
