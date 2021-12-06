49fc4ff9a0e3f9bd9550a3403b9f94a564eb5c65
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-06 07:24:46 +0530

    Added ISR to allEvents page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router";
 
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
-const AllEventsPage = () => {
-  const events = getAllEvents();
+const AllEventsPage = (props) => {
+  const { events } = props;
 
   const router = useRouter();
 
@@ -23,3 +23,14 @@ const AllEventsPage = () => {
 };
 
 export default AllEventsPage;
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
