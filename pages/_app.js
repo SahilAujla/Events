701113716e87cbf18088850df96e857e4c34a198
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-06 09:59:15 +0530

    Merging Head content

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Next Events</title>
+        {/* This is a comment and this title will be replaced by the individual titles of other pages because Next.js takes the latest title and renders it for a page only, however if there's any page that does not have a title then this title will be displayed for that page, same is the case for meta tage below*/}
+        <meta name="description" content="NextJS Events" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
