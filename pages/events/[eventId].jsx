fc943c9de3aff0c0a8f5f29951c4f5d4a15caf9e
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-06 07:08:09 +0530

    Added SSG to dynamic page of [eventId].

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,19 +1,14 @@
 import { Fragment } from "react";
 
-import { useRouter } from "next/router";
-
-import { getEventById } from "../../dummy-data";
+import { getEventById, getAllEvents } from "../../helpers/api-util";
 
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
 
-const EventDetailPage = () => {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+const EventDetailPage = (props) => {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -40,3 +35,27 @@ const EventDetailPage = () => {
 };
 
 export default EventDetailPage;
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const allEvents = await getAllEvents();
+  const pathNames = allEvents.map((event) => ({
+    params: { eventId: event.id },
+  }));
+
+  return {
+    paths: pathNames,
+    fallback: false,
+  };
+}
